af7b63410b74b9f9845972e9bd8c3d668bc92bc2
IAmSorrowful <97572520+IAmSorrowful@users.noreply.github.com>
2022-07-06 14:38:41 -0500

    Update 3415252509.lua

--- Games/3415252509.lua
+++ Games/3415252509.lua
@@ -1,3 +1,3 @@
 --Vyyrahk city
 --Retard
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ClearanceObvious/Script-Hub/main/PrisonLifeDONE.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ClearanceObvious/Script-Hub/main/Vyyrahk.lua", true))()
